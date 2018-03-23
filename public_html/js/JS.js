acf53adc6bfa722b8be753672d7a74f49c4934d2
David <David@localhost>
2018-03-23 11:05:43 -0300

    Atualização Dia 23/03/2018

--- public_html/js/JS.js
+++ public_html/js/JS.js
@@ -1,12 +1,19 @@
-$(document).ready(function(){
-     $('.carousel').carousel();
-     $('.carousel.carousel-slider').carousel({fullWidth: true});
-     
-     $(".indicadorR").click(function(){
-         $('.carousel').carousel('next');
-     });
-     
-     $(".indicadorL").click(function(){
-         $('.carousel').carousel('prev');
-     });
+$(document).ready(function () {
+    $(".carousel").carousel();
+    $(".carousel.carousel-slider").carousel({fullWidth: true});
+
+    $(".indicadorR").click(function () {
+        $('.carousel').carousel("next");
+    });
+
+    $(".indicadorL").click(function () {
+        $('.carousel').carousel("prev");
+    });
+    autoplay();
 });
+
+function autoplay(){
+    setTimeout(autoplay, 10000);
+    $(".carousel").carousel("next");
+}
+
